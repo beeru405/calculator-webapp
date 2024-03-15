361711fe7ae1900e05b833df15fbef7dfb567e05
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-03-15 12:03:57 +0530

    Update and rename src/test/java/mypackage/CalculatorSeleniumTest.java to src/test/java/CalculatorTest.java

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://192.168.138.114:8080/calculator");
+    }
+
+    @Test
+    public void testAddition() {
+        WebElement num1 = driver.findElement(By.name("num1"));
+        num1.sendKeys("5");
+
+        WebElement num2 = driver.findElement(By.name("num2"));
+        num2.sendKeys("3");
+
+        WebElement operator = driver.findElement(By.name("operator"));
+        operator.sendKeys("+");
+
+        WebElement calculateButton = driver.findElement(By.xpath("//input[@type='submit']"));
+        calculateButton.click();
+
+        WebElement result = driver.findElement(By.xpath("//h3[contains(text(), '8')]"));
+        assertTrue(result.isDisplayed());
+    }
+
+    // More test cases for subtraction, multiplication, and division
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
